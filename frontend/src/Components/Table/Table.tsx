11451623fbf504a8c63937b7afc76720efa31dc9
VedadOhran <vedad.ohran0@gmail.com>
2024-10-29 19:45:41 +0100

    implemented a features to display company informations directly within the dashboard

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -1,52 +1,36 @@
-import React from 'react';
-import { testIncomeStatementData } from './testData';
+type Props = {
+  config: any;
+  data: any;
+};
 
-
-const data = testIncomeStatementData;
-
-interface Props {}
-
-type Company = (typeof data)[0];
-
-const configs = [
-    {
-        label: 'Year',
-        render: (company: Company) => company.acceptedDate
-    },
-    {
-        label: 'Cost of Revenue',
-        render: (company: Company) => company.costOfRevenue
-    }
-]
-
-const Table = (props: Props) => {
-  const renderedRows = data.map((company) => {
+const Table = ({ config, data }: Props) => {
+  const renderedRows = data.map((company: any) => {
     return (
       <tr key={company.cik}>
-        {configs.map((val: any) => {
-          return (
-            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-              {val.render(company)}
-            </td>
-          );
+        {config.map((val: any) => {
+          return <td className="p-3">{val.render(company)}</td>;
         })}
-      </tr> 
+      </tr>
     );
   });
-  const renderedHeaders = configs.map((config:any) => {
+  const renderedHeaders = config.map((config: any) => {
     return (
-        <th className='p-4 text-left text-xs font-medium text-grey-500 uppercase tracking-wider'
-        key={config.label}>
-            {config.label}
-        </th>
-    )
-  })
-  return <div className='bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8'>
-    <table>
-        <thead className='min-w-full divide-y divide=gray-200 m-5'>{renderedHeaders}</thead>
+      <th
+        className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        key={config.label}
+      >
+        {config.label}
+      </th>
+    );
+  });
+  return (
+    <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+      <table className="min-w-full divide-y divide-gray-200 m-5">
+        <thead className="bg-gray-50">{renderedHeaders}</thead>
         <tbody>{renderedRows}</tbody>
-    </table>
-  </div>;
-}
+      </table>
+    </div>
+  );
+};
 
-export default Table
+export default Table;
